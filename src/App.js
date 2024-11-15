291bac1eca7d7680d44aac7de0af1377fe568b43
Siva Ganesh <sivagstandard@gmail.com>
2024-11-15 10:37:44 +0530

    Use state muptile updates

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   const [count,setCount]=useState(0);
   function addcount(){
-    setCount(count+1);
+    setCount(count => count+1);
+    setCount(count => count+1);
+    setCount(count => count+1);
     console.log(useState);
   }
   function subcount(){
