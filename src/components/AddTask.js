7253cf39e980c30a8853d44d415cf024b44992b8
Siva Ganesh <sivagstandard@gmail.com>
2024-12-11 17:02:17 +0530

    Input fields

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,13 +2,35 @@ import { useState } from 'react'
 import '../App.css'
 export  function AddTask() {
     const [addTask,setaddTask]=useState("");
+    const [progress,setProgress]=useState(false);
+    function clear(){
+      setaddTask("");
+      setProgress(false);
+
+    }
+    function addTasks(e){
+      e.preventDefault();
+      //console.log(typeof(progress))
+      const tasks={
+        id: Math.floor(Math.random()*100000),
+        name:addTask,
+        completed:Boolean(progress)
+      }
+      console.log(tasks);
+      clear();
+    }
   return (
     <div className='box'>
-      <form action="">
+      <form onSubmit={(e) => addTasks(e)}>
         <label htmlFor="task">Taskname:</label>
         <input type="text" onChange={(e) => setaddTask(e.target.value)} name="task" id="task" autoComplete='off' value={addTask}/>
+        <select onChange={(e) => setProgress(e.target.value)} value={progress}>
+          <option value={false}>Pending</option>
+          <option value={true}>Completed</option>
+        </select>
         <button type='Submit' className='addtask'>Add</button>
-        <span onClick={() => setaddTask("")} className='clear'>Clear</span>
+        <span onClick={() => clear() } className='clear'>Clear</span>
+
         <p>{addTask}</p>
       </form>
     </div>
