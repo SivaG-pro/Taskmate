5153b715d00e967725a74b7f311e6a95c8939441
Siva Ganesh <sivagstandard@gmail.com>
2024-12-12 11:36:13 +0530

    CSS update

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -2,8 +2,8 @@ export  function TaskCard({task,deletetask}) {
   return (
     <div>
       
-          <li className={task.completed ? "complete":"incomplete"} >
-            <span className="boxes">{task.id} - {task.name}
+          <li>
+            <span className={`boxes ${task.completed ? "complete":"incomplete"}`}>{task.id} - {task.name}
             <button className="sub" onClick={() => deletetask(task.id)}>Delete</button>
             </span>
           <br />
