59b5ec58a1a9161e41e72d52362f49f1e67346f0
Siva Ganesh <sivagstandard@gmail.com>
2024-12-23 19:04:03 +0530

    Complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
 import logo from "../assets/logo.png"
 import '../App.css'
+import { useState } from "react"
 
 export function Header() {
+  const [theme,settheme]=useState("dark")
+ // document.documentElement.classList.add(theme);
+
+   
   return (
     <div>
-      <img className="logo" src={logo}/>
+      <img className="logo" src={logo} alt=""/>
+      
       <a href="\" className="nav">Home</a>
+      <br />
+      {/* <div className="themeselect">
+      <span className="light"></span>
+      <span className="dark"></span>
+      </div> */}
     </div>
   )
 }
